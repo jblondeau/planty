673c3aa6c05fd6028c738d12a373569c0536f48d
jblondeau <julien.blondeau59@gmail.com>
2024-06-21 18:43:29 +0200

    modification functions.php pour validation du style

--- functions.php
+++ functions.php
@@ -1,16 +1,16 @@
 <?php
+
 /**
  * Theme functions and definitions
  *
  * @package HelloElementor
  */
 
- add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 
- function theme_enqueue_styles() {
- 
-	 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
- 
-  
- 
- }
+function theme_enqueue_styles()
+{
+
+	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+	wp_enqueue_style('child-styles', get_stylesheet_uri());
+}
