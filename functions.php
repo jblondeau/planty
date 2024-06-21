e1da5dda77310b832bcfa3f3cd47fc40a06d21fb
jblondeau <julien.blondeau59@gmail.com>
2024-06-21 15:11:34 +0200

    flush

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementor
+ */
+
+ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+ function theme_enqueue_styles() {
+ 
+	 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ 
+  
+ 
+ }
