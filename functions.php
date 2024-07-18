5c1a192874b2c4db6cc43603025f635b8e252755
jblondeau <julien.blondeau59@gmail.com>
2024-07-18 17:36:42 +0200

    hook

--- functions.php
+++ functions.php
@@ -10,7 +10,38 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 
 function theme_enqueue_styles()
 {
-
 	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 	wp_enqueue_style('child-styles', get_stylesheet_uri());
 }
+
+add_filter('wp_nav_menu_items', 'prefix_add_menu_item', 3, 2);
+/**
+ * Add Menu Item to a specific place in the menu
+ */
+function prefix_add_menu_item($items, $args)
+{
+	if (is_user_logged_in()) {
+		$items_array = array();
+		$items_array = explode('<li', $items);
+
+		array_splice($items_array, count($items_array) - 1, 0, '
+				 id = "menu-item-78" class="menu-item menu-item-type-custom menu-item-object-custom parent hfe-creative-menu">
+			<a href="http://planty.local/wp-admin" class="hfe-menu-item">Admin</a>
+	    </li>
+		'); // insert custom item after 9th item one
+
+
+
+		$items = implode('<li', $items_array);
+	}
+
+	return $items;
+}
+
+
+
+
+
+// <li id = "menu-item-78" class="menu-item menu-item-type-custom menu-item-object-custom parent hfe-creative-menu">
+// <a href="http://planty.local/wp-admin" class="hfe-menu-item">Admin</a>
+// </li>
