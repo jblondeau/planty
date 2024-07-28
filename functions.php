ef973f3d45bd88d689d5de88607267f2e1853bad
jblondeau <julien.blondeau59@gmail.com>
2024-07-28 13:52:39 +0200

    Correction admin menu dans function + correction responsive

--- functions.php
+++ functions.php
@@ -20,11 +20,13 @@ add_filter('wp_nav_menu_items', 'prefix_add_menu_item', 3, 2);
  */
 function prefix_add_menu_item($items, $args)
 {
-	if (is_user_logged_in()) { //si utilisateur connecté
-		$items_array = array();
-		$items_array = explode('<li', $items); //explose des li dans tableau
 
-		array_splice($items_array, count($items_array) - 1, 0, '
+	if (is_user_logged_in()) { //si utilisateur connecté
+		if ($args->menu == 'header') { // si on est dans le menu header
+			$items_array = array();
+			$items_array = explode('<li', $items); //explose des li dans tableau
+
+			array_splice($items_array, count($items_array) - 1, 0, '
 				 id = "menu-item-78" class="menu-item menu-item-type-custom menu-item-object-custom parent hfe-creative-menu">
 			<a href="http://planty.local/wp-admin" class="hfe-menu-item">Admin</a>
 	    </li>
@@ -32,7 +34,8 @@ function prefix_add_menu_item($items, $args)
 
 
 
-		$items = implode('<li', $items_array); // implode des li
+			$items = implode('<li', $items_array); // implode des li
+		}
 	}
 
 	return $items;
